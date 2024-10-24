ff82462090a9eae7c95c4439710db7147d77cf0c
DavasProgramacoes <davilucaspetry@gmail.com>
2024-10-24 07:55:08 -0300

    .

--- perguntas.js
+++ perguntas.js
@@ -1,17 +1,17 @@
 criaCartao(
-    'categoria',
-    'pergunta',
-    'resposta'
+    'matemastica',
+    'tinha 10 pessoas no culto, entraram mais 10 sairam 5, quantas pessoas no culto tinha ?',
+    '15? la ele'
 )
 
 criaCartao(
-    'categoria',
-    'pergunta',
-    'resposta'
+    'foloclore',
+    'tinham dois indiozinhos em um montinho de terra, um com tanga e outro sem tanga, qual dos dois indiozinhos tem terra na bunda ?',
+    'o sem tanga ( ͡° ͜ʖ ͡°)'
 )
 
 criaCartao(
-    'categoria',
-    'pergunta',
-    'resposta'
+    'comer cimentos gerais',
+    'como você quebrou o cano ?',
+    'qual cano? quebrei varios'
 )
